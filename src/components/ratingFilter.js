beb56799d164cbba3378d42d78b1b19e41f92a8c
houssemlouati <houssemeddine.louati@esprit.tn>
2020-04-19 16:45:47 +0100

    movie-app

---
+++ src/components/ratingFilter.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Rating from "./rating";
+
+const RatingFilter = ({ count, onchange }) => {
+  return (
+    <div>
+      <Rating count={count} change={onchange} />
+    </div>
+  );
+};
+export default RatingFilter;
